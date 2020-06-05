74c80fc2a5563e530d11dadac4090236e55b999f
Andrey Sokolov <Zokolof@yandex.ru>
2020-06-05 12:59:45 +0300

    .

--- include/nonlocal_influence_functions.hpp
+++ include/nonlocal_influence_functions.hpp
@@ -5,8 +5,36 @@
 #include <array>
 #include "power.hpp"
 
-namespace nonlocal::influence
-{
+namespace nonlocal::influence {
+
+class _elliptical_region {
+    _elliptical_region() noexcept = default;
+
+    template<class Type>
+    static std::array<Type, 3> calc_coeff(const std::array<Type, 2>& r, const Type theta = 0) noexcept {
+        const Type cos_theta = cos(theta), sin_theta = sin(theta);
+        return { math_meta::power<2>(cos_theta / r[0]) + math_meta::power<2>(sin_theta / r[1]),
+                                    2.*cos_theta*sin_theta * (1./(r[0]*r[0]) - 1./(r[1]*r[1])),
+                 math_meta::power<2>(sin_theta / r[0]) + math_meta::power<2>(cos_theta / r[1]) };
+    }
+
+    template<class Type>
+    static Type calc_ellipse(const std::array<Type, 3>& coeff,
+                             const std::array<Type, 2>& x, const std::array<Type, 2>& y) noexcept {
+        const Type dx = x[0] - x[1], dy = y[0] - y[1];
+        return coeff[0]*dx*dx + (coeff[1]*dx + coeff[2]*dy)*dy;
+    }
+
+public:
+    template<class Type>
+    friend class constant;
+
+    template<class Type, uint64_t p, uint64_t q>
+    friend class polynomial;
+
+    template<class Type>
+    friend class normal_distribution;
+};
 
 template<class Type>
 class constant {
@@ -29,16 +57,12 @@ public:
     }
 
     void set_parameters(const std::array<Type, 2>& r, const Type theta = 0) noexcept {
-        const Type cos_theta = cos(theta), sin_theta = sin(theta);
-        _coeff = { math_meta::power<2>(cos_theta / r[0]) + math_meta::power<2>(sin_theta / r[1]),
-                                      2.*cos_theta*sin_theta * (1./(r[0]*r[0]) - 1./(r[1]*r[1])),
-                   math_meta::power<2>(sin_theta / r[0]) + math_meta::power<2>(cos_theta / r[1]) };
+        _coeff = _elliptical_region::calc_coeff(r, theta);
         _norm = 1. / (M_PI * r[0] * r[1]);
     }
 
     Type operator()(Type xL, Type xNL, Type yL, Type yNL) const noexcept {
-        const Type dx = xL - xNL, dy = yL - yNL;
-        return _coeff[0]*dx*dx + (_coeff[1]*dx + _coeff[2]*dy)*dy < 1. ? _norm : 0.;
+        return _elliptical_region::calc_ellipse(_coeff, {xL, xNL}, {yL, yNL}) < 1. ? _norm : 0.;
     }
 };
 
@@ -65,15 +89,12 @@ public:
     }
 
     void set_parameters(const std::array<Type, 2>& r, const Type theta = 0) noexcept {
-        const Type cos_theta = cos(theta), sin_theta = sin(theta);
-        _coeff = { math_meta::power<2>(cos_theta / r[0]) + math_meta::power<2>(sin_theta / r[1]),
-                                      2.*cos_theta*sin_theta * (1./(r[0]*r[0]) - 1./(r[1]*r[1])),
-                   math_meta::power<2>(sin_theta / r[0]) + math_meta::power<2>(cos_theta / r[1]) };
+        _coeff = _elliptical_region::calc_coeff(r, theta);
         _norm = p / ((M_PI + M_PI) * r[0] * r[1] * std::beta(2./p, q+1.));
     }
 
     Type operator()(Type xL, Type xNL, Type yL, Type yNL) const noexcept {
-        const Type dx = xL - xNL, dy = yL - yNL, h = _coeff[0]*dx*dx + (_coeff[1]*dx + _coeff[2]*dy)*dy;
+        const Type h = _elliptical_region::calc_ellipse(_coeff, {xL, xNL}, {yL, yNL});
         if constexpr (p % 2)
             return h > 1 ? 0. : _norm * math_meta::power<q>(1. - math_meta::power<p/2>(h)*sqrt(h));
         else
@@ -85,20 +106,29 @@ template<class Type>
 class normal_distribution {
     static_assert(std::is_floating_point_v<Type>, "The Type must be floating point.");
 
-    Type _rr = 0, _norm = 0;
+    std::array<Type, 3> _coeff = {};
+    Type _norm = 0;
 
 public:
     normal_distribution(const Type r) noexcept {
         set_parameters(r);
     }
 
+    normal_distribution(const std::array<Type, 2>& r, const Type theta = 0) noexcept {
+        set_parameters(r, theta);
+    }
+
     void set_parameters(const Type r) noexcept {
-        _rr = r*r;
-        _norm = 1. / (M_PI * _rr);
+        set_parameters({r, r});
+    }
+
+    void set_parameters(const std::array<Type, 2>& r, const Type theta = 0) noexcept {
+        _coeff = _elliptical_region::calc_coeff(r, theta);
+        _norm = 1. / (M_PI * r[0] * r[1]);
     }
 
     Type operator()(Type xL, Type xNL, Type yL, Type yNL) const noexcept {
-        return _norm * exp(-(math_meta::power<2>(xL - xNL) + math_meta::power<2>(yL - yNL)) / _rr);
+        return _norm * exp(-_elliptical_region::calc_ellipse(_coeff, {xL, xNL}, {yL, yNL}));
     }
 };
 
