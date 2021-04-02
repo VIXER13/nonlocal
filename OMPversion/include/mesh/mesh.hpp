9be504d5b29f9bcbe4ac86abb80677f0477be317
andrewSKLV <andrewsklv@oxygen>
2021-04-02 20:51:36 +0300

    Удалил устаревшую OpenMP версию

--- OMPversion/include/mesh/mesh.hpp
+++
@@ -1,7 +0,0 @@
-#ifndef MESH_HPP
-#define MESH_HPP
-
-#include "su2_parser.hpp"
-#include "save_results.hpp"
-
-#endif
--- OMPversion/include/metamath/metamath.hpp
+++
@@ -1,7 +0,0 @@
-#ifndef METAMATH_HPP
-#define METAMATH_HPP
-
-#include "finite_element/finite_element.hpp"
-#include "power.hpp"
-
-#endif
