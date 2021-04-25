002021a4dc197ee098c6ab175797c765462c8690
andrewSKLV <andrewsklv@oxygen>
2021-04-25 22:15:04 +0300

    metamath/finite_element add derivative_base

--- include/metamath/finite_element/element_2d/element_2d.hpp
+++ include/metamath/finite_element/element_2d/element_2d.hpp
@@ -2,8 +2,7 @@
 #define FINITE_ELEMENT_2D_HPP
 
 #include "element_2d_base.hpp"
-#include "derivative.hpp"
-#include "to_function.hpp"
+#include "derivative_element_2d_basis.hpp"
 
 namespace metamath::finite_element {
 
@@ -11,30 +10,19 @@ namespace metamath::finite_element {
 // Таким образом пользователь сможет добавлять свои реализации конечных элементов не прибегая к дублированию интерфейса.
 template<class T, template<class> class Element_Type>
 class element_2d : public virtual element_2d_base<T>,
-                   public Element_Type<T> {
-    using Element_Type<T>::xi;
-    using Element_Type<T>::eta;
-    using Element_Type<T>::nodes;
-    using Element_Type<T>::basis;
-
-    static_assert(std::tuple_size<decltype(basis)>::value == nodes.size(), "The number of functions and nodes does not match.");
-
-protected:
-    static inline const std::array<std::function<T(const std::array<T, 2>&)>, nodes.size()>
-        _N    = symdiff::to_function<T, 2>(basis),
-        _Nxi  = symdiff::to_function<T, 2>(symdiff::derivative<xi>(basis)),
-        _Neta = symdiff::to_function<T, 2>(symdiff::derivative<eta>(basis));
+                   public derivative_element_2d_basis<T, Element_Type, 2> {
+    using derivative_base = derivative_element_2d_basis<T, Element_Type, 2>;
 
 public:
     ~element_2d() override = default;
 
-    size_t nodes_count() const override { return _N.size(); }
+    size_t nodes_count() const override { return derivative_base::N.size(); }
 
-    const std::array<T, 2>& node(const size_t i) const override { return nodes[i]; }
+    const std::array<T, 2>& node(const size_t i) const override { return Element_Type<T>::nodes[i]; }
 
-    T N   (const size_t i, const std::array<T, 2>& xi) const override { return _N   [i](xi); }
-    T Nxi (const size_t i, const std::array<T, 2>& xi) const override { return _Nxi [i](xi); }
-    T Neta(const size_t i, const std::array<T, 2>& xi) const override { return _Neta[i](xi); }
+    T N   (const size_t i, const std::array<T, 2>& xi) const override { return derivative_base::N   [i](xi); }
+    T Nxi (const size_t i, const std::array<T, 2>& xi) const override { return derivative_base::Nxi [i](xi); }
+    T Neta(const size_t i, const std::array<T, 2>& xi) const override { return derivative_base::Neta[i](xi); }
 
     T boundary(const side_2d bound, const T x) const override { return Element_Type<T>::boundary(bound, x); }
 };
--- include/metamath/finite_element/element_2d/element_2d_serendipity.hpp
+++ include/metamath/finite_element/element_2d/element_2d_serendipity.hpp
@@ -9,31 +9,20 @@ namespace metamath::finite_element {
 
 template<class T, template<class> class Element_Type>
 class element_2d_serendipity : public virtual element_2d_base<T>,
-                               public Element_Type<T> {
-    using Element_Type<T>::xi;
-    using Element_Type<T>::eta;
+                               public derivative_element_2d_basis<T, Element_Type, 3> {
+    using derivative_base = derivative_element_2d_basis<T, Element_Type, 3>;
     using Element_Type<T>::_p;
-    using Element_Type<T>::nodes;
-    using Element_Type<T>::basis;
-
-    static_assert(std::tuple_size<decltype(basis)>::value == nodes.size(), "The number of functions and nodes does not match.");
-
-protected:
-    static inline const std::array<std::function<T(const std::array<T, 3>&)>, nodes.size()>
-        _N    = symdiff::to_function<T, 3>(basis),
-        _Nxi  = symdiff::to_function<T, 3>(symdiff::derivative<xi>(basis)),
-        _Neta = symdiff::to_function<T, 3>(symdiff::derivative<eta>(basis));
 
 public:
     ~element_2d_serendipity() override = default;
 
-    size_t nodes_count() const override { return _N.size(); }
+    size_t nodes_count() const override { return derivative_base::N.size(); }
 
-    const std::array<T, 2>& node(const size_t i) const override { return nodes[i]; }
+    const std::array<T, 2>& node(const size_t i) const override { return Element_Type<T>::nodes[i]; }
 
-    T N   (const size_t i, const std::array<T, 2>& xi) const override { return _N   [i]({xi[0], xi[1], _p}); }
-    T Nxi (const size_t i, const std::array<T, 2>& xi) const override { return _Nxi [i]({xi[0], xi[1], _p}); }
-    T Neta(const size_t i, const std::array<T, 2>& xi) const override { return _Neta[i]({xi[0], xi[1], _p}); }
+    T N   (const size_t i, const std::array<T, 2>& xi) const override { return derivative_base::N   [i]({xi[0], xi[1], _p}); }
+    T Nxi (const size_t i, const std::array<T, 2>& xi) const override { return derivative_base::Nxi [i]({xi[0], xi[1], _p}); }
+    T Neta(const size_t i, const std::array<T, 2>& xi) const override { return derivative_base::Neta[i]({xi[0], xi[1], _p}); }
 
     T boundary(const side_2d bound, const T x) const override { return Element_Type<T>::boundary(bound, x); }
 };
