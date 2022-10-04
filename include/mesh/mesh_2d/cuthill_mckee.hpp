cd8dafb74539ee6aa18421fdc855ba66ae537ff4
Andrey Sokolov <Zokolof@yandex.ru>
2022-10-04 15:34:46 +0300

    small changes in initializers in cuthill_mkee algorithm

--- include/mesh/mesh_2d/cuthill_mckee.hpp
+++ include/mesh/mesh_2d/cuthill_mckee.hpp
@@ -20,10 +20,6 @@ class _cuthill_mckee final {
         explicit initializer_base(const size_t size)
             : _is_include(size, false) {}
 
-        size_t nodes_count() const noexcept {
-            return _is_include.size();
-        }
-
         bool check_neighbour(const size_t node, const size_t neighbour) {
             const bool result = node != neighbour && !_is_include[neighbour];
             if (result)
@@ -39,42 +35,40 @@ class _cuthill_mckee final {
 
     template<class I>
     class shifts_initializer final : public initializer_base {
+        std::vector<I>& _shifts;
+
     public:
-        explicit shifts_initializer(const size_t size)
-            : initializer_base{size} {}
+        explicit shifts_initializer(std::vector<I>& shifts)
+            : initializer_base{shifts.size() - 1}
+            , _shifts{shifts} {}
 
-        std::vector<I> init_data_vector() const {
-            return std::vector<I>(nodes_count() + 1, 0);
-        }
-
-        void add(std::vector<I>& shifts, const size_t node, const size_t neighbour) {
+        void add(const size_t node, const size_t neighbour) {
             if (check_neighbour(node, neighbour))
-                ++shifts[node + 1];
+                ++_shifts[node + 1];
         }
     };
 
     template<class I>
     class indices_initializer final : public initializer_base {
         const std::vector<I>& _shifts;
+        std::vector<I>& _indices;
         I _node_shift = 0;
 
     public:
-        explicit indices_initializer(const std::vector<I>& shifts)
+        explicit indices_initializer(std::vector<I>& indices,
+                                     const std::vector<I>& shifts)
             : initializer_base{shifts.size() - 1}
-            , _shifts{shifts} {}
+            , _shifts{shifts}
+            , _indices{indices} {}
 
         void reset_include() {
             _node_shift = 0;
             initializer_base::reset_include();
         }
 
-        std::vector<I> init_data_vector() const {
-            return std::vector<I>(_shifts.back(), 0);
-        }
-
-        void add(std::vector<I>& indices, const size_t node, const size_t neighbour) {
+        void add(const size_t node, const size_t neighbour) {
             if (check_neighbour(node, neighbour)) {
-                indices[_shifts[node] + _node_shift] = neighbour;
+                _indices[_shifts[node] + _node_shift] = neighbour;
                 ++_node_shift;
             }
         }
@@ -96,32 +90,32 @@ class _cuthill_mckee final {
     }
 
     template<theory_t Theory, class T, class I, class Initializer>
-    static std::vector<I> init_vector(const mesh_proxy<T, I>& mesh, Initializer&& initializer) {
-        std::vector<I> data_vector = initializer.init_data_vector();
-#pragma omp parallel for default(none) shared(mesh, data_vector) firstprivate(initializer)
+    static void init_vector(const mesh_proxy<T, I>& mesh, Initializer&& initializer) {
+#pragma omp parallel for default(none) shared(mesh) firstprivate(initializer)
         for(size_t node = mesh.first_node(); node < mesh.last_node(); ++node) {
             initializer.reset_include();
             for(const I eL : mesh.nodes_elements_map(node)) {
                 if constexpr (Theory == theory_t::LOCAL)
                     for(size_t jL = 0; jL < mesh.mesh().nodes_count(eL); ++jL)
-                        initializer.add(data_vector, node, mesh.mesh().node_number(eL, jL));
+                        initializer.add(node, mesh.mesh().node_number(eL, jL));
                 if constexpr (Theory == theory_t::NONLOCAL)
                     for(const I eNL : mesh.neighbors(eL))
                         for(size_t jNL = 0; jNL < mesh.mesh().nodes_count(eNL); ++jNL)
-                            initializer.add(data_vector, node, mesh.mesh().node_number(eNL, jNL));
+                            initializer.add(node, mesh.mesh().node_number(eNL, jNL));
             }
         }
-        return data_vector;
     }
 
     template<class T, class I>
     static node_graph<I> init_graph(const mesh_proxy<T, I>& mesh, const bool is_nonlocal) {
         _cuthill_mckee::node_graph<I> graph;
-        graph.shifts = is_nonlocal ? init_vector<theory_t::NONLOCAL>(mesh, shifts_initializer<I>{mesh.mesh().nodes_count()})
-                                   : init_vector<theory_t::   LOCAL>(mesh, shifts_initializer<I>{mesh.mesh().nodes_count()});
+        graph.shifts.resize(mesh.mesh().nodes_count() + 1, 0);
+        if (is_nonlocal) init_vector<theory_t::NONLOCAL>(mesh, shifts_initializer<I>{graph.shifts});
+        else             init_vector<theory_t::   LOCAL>(mesh, shifts_initializer<I>{graph.shifts});
         prepare_shifts(graph.shifts);
-        graph.indices = is_nonlocal ? init_vector<theory_t::NONLOCAL>(mesh, indices_initializer{graph.shifts})
-                                    : init_vector<theory_t::   LOCAL>(mesh, indices_initializer{graph.shifts});
+        graph.indices.resize(graph.shifts.back());
+        if (is_nonlocal) init_vector<theory_t::NONLOCAL>(mesh, indices_initializer<I>{graph.indices, graph.shifts});
+        else             init_vector<theory_t::   LOCAL>(mesh, indices_initializer<I>{graph.indices, graph.shifts});
         return graph;
     }
 
