eca476945aea1fcb4e0b2217dbd193a60614307b
VIXER <Zokolof@yandex.ru>
2023-01-19 01:09:19 +0300

    Fixes access to methods in finite_elements Added tests for elements

--- libraries/metamath/finite_elements/finite_elements_2d/finite_elements_2d.hpp
+++ libraries/metamath/finite_elements/finite_elements_2d/finite_elements_2d.hpp
@@ -4,8 +4,6 @@
 #include "geometry/geometry_2d.hpp"
 #include "geometry/geometric_primitives/geometric_primitives_2d.hpp"
 
-#include "element/element_2d.hpp"
-#include "element/element_2d_serendipity.hpp"
 #include "element/element_2d_integrate.hpp"
 #include "element/basis/basis_2d.hpp"
 
