c6e41f71fe605df5d38f48906b5511cf35d4686d
Andrey Sokolov <Zokolof@yandex.ru>
2023-01-27 01:17:27 +0300

    (-) fix initializing uniform ranges

--- libraries/parallel_utils/init_uniform_ranges.cpp
+++ libraries/parallel_utils/init_uniform_ranges.cpp
@@ -3,7 +3,7 @@
 namespace parallel_utils {
 
 std::vector<std::ranges::iota_view<size_t, size_t>> init_uniform_ranges(const size_t size, const size_t count) {
-    std::vector<std::ranges::iota_view<size_t, size_t>> ranges(size);
+    std::vector<std::ranges::iota_view<size_t, size_t>> ranges(count);
     for(const size_t i : std::ranges::iota_view{0u, count})
         ranges[i] = { size / ranges.size() *  i,
                       size / ranges.size() * (i + 1) + (i == ranges.size() - 1) * size % ranges.size() };
