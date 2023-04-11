f0974f5816120d9cf71dd74f4f5b3fdc623be3d8
Andrey Sokolov <Zokolof@yandex.ru>
2023-04-11 16:18:32 +0300

    Removed throw check function and replaced with EXPECT_TROW macro

--- tests/config/required_fields.cpp
+++ tests/config/required_fields.cpp
@@ -24,131 +24,101 @@ using mesh_data_2d = mesh_data<2u>;
 
 const Json::Value tests = read_json(required_fields_json_data, std::next(required_fields_json_data, required_fields_json_size));
 
-template<class Data>
-bool exception_catched(const Json::Value& value) {
-    try {
-        const Data data{value};
-    } catch(const std::exception&) {
-        return true;
-    }
-    return false;
-}
-
 }
 
 
 
-TEST(config_required_fields, nonstationary_missed_all)
-{
-    EXPECT_TRUE(exception_catched<nonstationary_data<float>>(tests["nonstationary_missed_all"]));
+TEST(config_required_fields, nonstationary_missed_all) {
+    EXPECT_THROW(nonstationary_data<float>{tests["nonstationary_missed_all"]}, std::exception);
 }
 
-TEST(config_required_fields, nonstationary_missed_time_step)
-{
-    EXPECT_TRUE(exception_catched<nonstationary_data<float>>(tests["nonstationary_missed_time_step"]));
+TEST(config_required_fields, nonstationary_missed_time_step) {
+    EXPECT_THROW(nonstationary_data<float>{tests["nonstationary_missed_time_step"]}, std::exception);
 }
 
-TEST(config_required_fields, nonstationary_missed_steps_count)
-{
-    EXPECT_TRUE(exception_catched<nonstationary_data<float>>(tests["nonstationary_missed_steps_count"]));
+TEST(config_required_fields, nonstationary_missed_steps_count) {
+    EXPECT_THROW(nonstationary_data<float>{tests["nonstationary_missed_steps_count"]}, std::exception);
 }
 
-TEST(config_required_fields, nonstationary_all_required_exists)
-{
-    EXPECT_FALSE(exception_catched<nonstationary_data<float>>(tests["nonstationary_all_required_exists"]));
+TEST(config_required_fields, nonstationary_all_required_exists) {
+    EXPECT_NO_THROW(nonstationary_data<float>{tests["nonstationary_all_required_exists"]});
 }
 
 
 
-TEST(config_required_fields, model_data_1d_missed_all)
-{
-    EXPECT_TRUE(exception_catched<model_data_1d>(tests["model_data_1d_missed_all"]));
+TEST(config_required_fields, model_data_1d_missed_all) {
+    EXPECT_THROW(model_data_1d{tests["model_data_1d_missed_all"]}, std::exception);
 }
 
-TEST(config_required_fields, model_data_1d_missed_local_weight)
-{
-    EXPECT_TRUE(exception_catched<model_data_1d>(tests["model_data_1d_missed_local_weight"]));
+TEST(config_required_fields, model_data_1d_missed_local_weight) {
+    EXPECT_THROW(model_data_1d{tests["model_data_1d_missed_local_weight"]}, std::exception);
 }
 
-TEST(config_required_fields, model_data_1d_missed_nonlocal_radius)
-{
-    EXPECT_TRUE(exception_catched<model_data_1d>(tests["model_data_1d_missed_nonlocal_radius"]));
+TEST(config_required_fields, model_data_1d_missed_nonlocal_radius) {
+    EXPECT_THROW(model_data_1d{tests["model_data_1d_missed_nonlocal_radius"]}, std::exception);
 }
 
 TEST(config_required_fields, model_data_1d_all_required_exists)
 {
-    EXPECT_FALSE(exception_catched<model_data_1d>(tests["model_data_1d_all_required_exists"]));
+    EXPECT_NO_THROW(model_data_1d{tests["model_data_1d_all_required_exists"]});
 }
 
 
 
-TEST(config_required_fields, model_data_2d_missed_all)
-{
-    EXPECT_TRUE(exception_catched<model_data_2d>(tests["model_data_2d_missed_all"]));
+TEST(config_required_fields, model_data_2d_missed_all) {
+    EXPECT_THROW(model_data_2d{tests["model_data_2d_missed_all"]}, std::exception);
 }
 
-TEST(config_required_fields, model_data_2d_missed_local_weight)
-{
-    EXPECT_TRUE(exception_catched<model_data_2d>(tests["model_data_2d_missed_local_weight"]));
+TEST(config_required_fields, model_data_2d_missed_local_weight) {
+    EXPECT_THROW(model_data_2d{tests["model_data_2d_missed_local_weight"]}, std::exception);
 }
 
-TEST(config_required_fields, model_data_2d_missed_nonlocal_radius)
-{
-    EXPECT_TRUE(exception_catched<model_data_2d>(tests["model_data_2d_missed_nonlocal_radius"]));
+TEST(config_required_fields, model_data_2d_missed_nonlocal_radius) {
+    EXPECT_THROW(model_data_2d{tests["model_data_2d_missed_nonlocal_radius"]}, std::exception);
 }
 
-TEST(config_required_fields, model_data_2d_all_required_exists)
-{
-    EXPECT_FALSE(exception_catched<model_data_2d>(tests["model_data_2d_all_required_exists"]));
+TEST(config_required_fields, model_data_2d_all_required_exists) {
+    EXPECT_NO_THROW(model_data_2d{tests["model_data_2d_all_required_exists"]});
 }
 
 
 
-TEST(config_required_fields, material_data_1d_missed_all)
-{
-    EXPECT_TRUE(exception_catched<material_data_1d>(tests["material_data_1d_missed_all"]));
+TEST(config_required_fields, material_data_1d_missed_all) {
+    EXPECT_THROW(material_data_1d{tests["material_data_1d_missed_all"]}, std::exception);
 }
 
-TEST(config_required_fields, material_data_1d_missed_elements_count)
-{
-    EXPECT_TRUE(exception_catched<material_data_1d>(tests["material_data_1d_missed_elements_count"]));
+TEST(config_required_fields, material_data_1d_missed_elements_count) {
+    EXPECT_THROW(material_data_1d{tests["material_data_1d_missed_elements_count"]}, std::exception);
 }
 
-TEST(config_required_fields, material_data_1d_missed_length)
-{
-    EXPECT_TRUE(exception_catched<material_data_1d>(tests["material_data_1d_missed_length"]));
+TEST(config_required_fields, material_data_1d_missed_length) {
+    EXPECT_THROW(material_data_1d{tests["material_data_1d_missed_length"]}, std::exception);
 }
 
-TEST(config_required_fields, material_data_1d_missed_physical)
-{
-    EXPECT_TRUE(exception_catched<material_data_1d>(tests["material_data_1d_missed_physical"]));
+TEST(config_required_fields, material_data_1d_missed_physical) {
+    EXPECT_THROW(material_data_1d{tests["material_data_1d_missed_physical"]}, std::exception);
 }
 
-TEST(config_required_fields, material_data_1d_all_required_exists)
-{
-    EXPECT_FALSE(exception_catched<material_data_1d>(tests["material_data_1d_all_required_exists"]));
+TEST(config_required_fields, material_data_1d_all_required_exists) {
+    EXPECT_NO_THROW(material_data_1d{tests["material_data_1d_all_required_exists"]});
 }
 
 
 
-TEST(config_required_fields, material_data_2d_missed_all)
-{
-    EXPECT_TRUE(exception_catched<material_data_2d>(tests["material_data_2d_missed_all"]));
+TEST(config_required_fields, material_data_2d_missed_all) {
+    EXPECT_THROW(material_data_2d{tests["material_data_2d_missed_all"]}, std::exception);
 }
 
-TEST(config_required_fields, material_data_2d_all_required_exists)
-{
-    EXPECT_FALSE(exception_catched<material_data_2d>(tests["material_data_2d_all_required_exists"]));
+TEST(config_required_fields, material_data_2d_all_required_exists) {
+    EXPECT_NO_THROW(material_data_2d{tests["material_data_2d_all_required_exists"]});
 }
 
 
 
-TEST(config_required_fields, mesh_data_2d_missed_all)
-{
-    EXPECT_TRUE(exception_catched<mesh_data_2d>(tests["mesh_data_2d_missed_all"]));
+TEST(config_required_fields, mesh_data_2d_missed_all) {
+    EXPECT_THROW(mesh_data_2d{tests["mesh_data_2d_missed_all"]}, std::exception);
 }
 
-TEST(config_required_fields, mesh_data_2d_all_required_exists)
-{
-    EXPECT_FALSE(exception_catched<mesh_data_2d>(tests["mesh_data_2d_all_required_exists"]));
+TEST(config_required_fields, mesh_data_2d_all_required_exists) {
+    EXPECT_NO_THROW(mesh_data_2d{tests["mesh_data_2d_all_required_exists"]});
 }
