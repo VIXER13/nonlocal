2406b2f3b4b3feac4ece26885ab7c886fe5d6e23
Andrey Sokolov <Zokolof@yandex.ru>
2023-11-02 00:45:41 +0300

    accidentally added directory deleted

--- test/embedded_files/required_fields_json.h
+++
@@ -1,6 +0,0 @@
-#ifndef required_fields_json_H
-#define required_fields_json_H
-#include <stddef.h>
-extern const char required_fields_json_data[];
-extern const size_t required_fields_json_size;
-#endif
