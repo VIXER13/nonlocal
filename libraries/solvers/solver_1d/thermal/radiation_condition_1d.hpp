b7d5dc207902e4817d96f727fce5c36e8cb7a5f9
_Safronov_Yurii_ <Safronov641@mail.ru>
2024-05-06 17:38:23 +0300

    developed 1D thermal radiation boundary condition, added corresponding unit test

--- libraries/solvers/solver_1d/thermal/radiation_condition_1d.hpp
+++ libraries/solvers/solver_1d/thermal/radiation_condition_1d.hpp
@@ -9,19 +9,28 @@ namespace nonlocal::thermal {
 
 template<class T, class I>
 void radiation_condition_1d(Eigen::SparseMatrix<T, Eigen::RowMajor, I>& matrix,
+                            Eigen::Matrix<T, Eigen::Dynamic, 1>& f,
                             const thermal_boundary_condition_1d<T>& boundary_condition,
+                            const Eigen::Matrix<T, Eigen::Dynamic, 1>& temperature_prev,
                             const T time_step, const size_t index) {
-    if (const auto* const condition = dynamic_cast<const radiation_1d<T>*>(&boundary_condition))
-        matrix.coeffRef(index, index) += time_step * condition->radiation();
+    static constexpr T STEFAN_BOLTZMANN_CONSTANT_X4 = T{4} * STEFAN_BOLTZMANN_CONSTANT<T>;
+    static constexpr T STEFAN_BOLTZMANN_CONSTANT_X3 = T{3} * STEFAN_BOLTZMANN_CONSTANT<T>;
+    if (const auto* const condition = dynamic_cast<const radiation_1d<T>*>(&boundary_condition)) {
+        const T temperature_pow_3 = metamath::functions::power<3>(temperature_prev[index]);
+        matrix.coeffRef(index, index) += time_step * condition->emissivity() * STEFAN_BOLTZMANN_CONSTANT_X4 * temperature_pow_3;
+        f[index] += time_step * condition->emissivity() * STEFAN_BOLTZMANN_CONSTANT_X3 * temperature_pow_3 * temperature_prev[index];
+    }
 }
 
 template<class T, class I>
 void radiation_condition_1d(Eigen::SparseMatrix<T, Eigen::RowMajor, I>& matrix,
-                            const thermal_boundaries_conditions_1d<T>& boundaries_conditions,
+                            Eigen::Matrix<T, Eigen::Dynamic, 1>& f,
+                            const thermal_boundaries_conditions_1d<T>& boundaries_conditions, 
+                            const Eigen::Matrix<T, Eigen::Dynamic, 1>& temperature_prev, 
                             const T time_step) {
     const std::array<size_t, 2> indices = {0, size_t(matrix.outerSize() - 1)};
     for(const size_t b : std::ranges::iota_view{0u, 2u})
-        radiation_condition_1d(matrix, *boundaries_conditions[b], time_step, indices[b]);
+        radiation_condition_1d(matrix, f, *boundaries_conditions[b], temperature_prev, time_step, indices[b]);
 }
 
 }
