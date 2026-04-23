b99e0f37beeebd53f0ae7763ca7a4c84b7956bbe
VIXER13 <Zokolof@yandex.ru>
2026-04-23 15:03:57 +0300

    (=) Simplified conductivity and thermal_strain evaluations

--- libraries/solvers/solver_2d/mechanical/evaluate_mechanical_parameters.hpp
+++ libraries/solvers/solver_2d/mechanical/evaluate_mechanical_parameters.hpp
@@ -24,12 +24,12 @@ evaluated_mechanical_parameters<T> evaluate_mechanical_parameters(const mesh::me
                 .container = std::vector<Hooke>(qshifts.size()),
                 .shift = qshifts.front()
             };
-            for(size_t q = qshifts.front(); q <= qshifts.back(); ++q)
-                result[q] = elastic.hooke(mesh.quad_coord(q));
+            for(const size_t qshift : qshifts)
+                result[qshift] = elastic.hooke(mesh.quad_coord(qshift));
             return result;
         }, parameter.physical.elastic);
 
-        if (temperature.empty() || std::holds_alternative<std::monostate>(parameter.physical.thermal_expansion))
+        if (temperature.empty())
             continue;
 
         physical.thermal_strain = std::visit(metamath::types::visitor{
@@ -40,41 +40,41 @@ evaluated_mechanical_parameters<T> evaluate_mechanical_parameters(const mesh::me
                     result.strain[qshift] = temperature[qshift] * evaluate<T, 2u>(thermal_expansion, mesh.quad_coord(qshift), {});
                 return result;
             },
-            [&mesh, &name, &temperature](const raw_orthotropic_thermal_expansion_t<T>& thermal_expansion) -> evaluated_thermal_strain_t<T> {
-                const auto qshifts = mesh.quad_shifts(name);
-                if (is_constant(thermal_expansion)) {
-                    orthotropic_constant_thermal_strain<T> result{
-                        .delta_temperature = metamath::types::vector_with_shifted_index<T>{std::vector<T>(qshifts.size(), T{0}), qshifts.front()},
-                        .thermal_expansion = evaluate<T, 2u>(thermal_expansion, {}, {})
+            [&mesh, &name, &temperature](const auto& thermal_expansion) -> evaluated_thermal_strain_t<T> {
+                using Expansion = std::remove_cvref_t<decltype(thermal_expansion)>;
+                if constexpr (std::is_same_v<Expansion, std::monostate>)
+                    return std::monostate{};
+                else {
+                    const auto qshifts = mesh.quad_shifts(name);
+                    if (is_constant(thermal_expansion)) {
+                        std::conditional_t<std::is_same_v<Expansion, raw_orthotropic_thermal_expansion_t<T>>,
+                            orthotropic_constant_thermal_strain<T>,
+                            anisotropic_constant_thermal_strain<T>
+                        > result = {
+                            .delta_temperature = metamath::types::vector_with_shifted_index<T>{
+                                .container = std::vector<T>(std::next(temperature.begin(), qshifts.front()), 
+                                                            std::next(temperature.begin(), qshifts.back() + 1)),
+                                .shift = qshifts.front()
+                            },
+                            .thermal_expansion = evaluate<T, 2u>(thermal_expansion, {}, {})
+                        };
+                        return result;
+                    }
+                    using namespace metamath::functions;
+                    std::conditional_t<std::is_same_v<Expansion, raw_orthotropic_thermal_expansion_t<T>>,
+                        orthotropic_thermal_strain<T>,
+                        anisotropic_thermal_strain<T>
+                    > result = {
+                        .strain = {
+                            .container = std::vector<std::array<T, std::tuple_size_v<Expansion>>>(qshifts.size()),
+                            .shift = qshifts.front()
+                        } 
                     };
                     for(const size_t qshift : qshifts)
-                        result.delta_temperature[qshift] = temperature[qshift];
+                        result.strain[qshift] = temperature[qshift] * evaluate<T, 2u>(thermal_expansion, mesh.quad_coord(qshift), {});
                     return result;
                 }
-                using namespace metamath::functions;
-                orthotropic_thermal_strain<T> result{ .strain = {std::vector<std::array<T, 2>>(qshifts.size()), qshifts.front()} };
-                for(const size_t qshift : qshifts)
-                    result.strain[qshift] = temperature[qshift] * evaluate<T, 2u>(thermal_expansion, mesh.quad_coord(qshift), {});
-                return result;
-            },
-            [&mesh, &name, &temperature](const raw_anisotropic_thermal_expansion_t<T>& thermal_expansion) -> evaluated_thermal_strain_t<T> {
-                const auto qshifts = mesh.quad_shifts(name);
-                if (is_constant(thermal_expansion)) {
-                    anisotropic_constant_thermal_strain<T> result{
-                        .delta_temperature = metamath::types::vector_with_shifted_index<T>{std::vector<T>(qshifts.size(), T{0}), qshifts.front()},
-                        .thermal_expansion = evaluate<T, 2u>(thermal_expansion, {}, {})
-                    };
-                    for(const size_t qshift : qshifts)
-                        result.delta_temperature[qshift] = temperature[qshift];
-                    return result;
-                }
-                using namespace metamath::functions;
-                anisotropic_thermal_strain<T> result{ .strain = {std::vector<std::array<T, 3>>(qshifts.size()), qshifts.front()} };
-                for(const size_t qshift : qshifts)
-                    result.strain[qshift] = temperature[qshift] * evaluate<T, 2u>(thermal_expansion, mesh.quad_coord(qshift), {});
-                return result;
-            },
-            [](auto&&) -> evaluated_thermal_strain_t<T> { throw std::domain_error{"Unexpected thermal_expansion variant state."}; }
+            }
         }, parameter.physical.thermal_expansion);
     }
     return result;
--- libraries/solvers/solver_2d/thermal/evaluate_conductivity.hpp
+++ libraries/solvers/solver_2d/thermal/evaluate_conductivity.hpp
@@ -11,39 +11,23 @@ evaluated_conductivity_2d<T> evaluate_conductivity(const mesh::mesh_2d<T, I>& me
                                                    const parameters_2d<T>& parameters,
                                                    const std::vector<T>& solution) {
     evaluated_conductivity_2d<T> conductivity;
-    for (const auto& [name, parameter] : parameters) {
-
-        auto conduct = std::visit(metamath::types::visitor{
-            [&mesh, &name, &solution](const raw_isotropic_conductivity_t<T>& conductivity) -> evaluated_conductivity_t<T> {
-                if (is_constant(conductivity))
-                    return std::get<T>(conductivity);
-                std::vector<T> result;
-                const auto qshifts = mesh.quad_shifts(name);
-                result.reserve(qshifts.size());
-                for(const size_t qshift: qshifts)
-                    result.push_back(evaluate<T, 2u>(conductivity, mesh.quad_coord(qshift), solution[qshift]));
-                return metamath::types::vector_with_shifted_index<T>{std::move(result), qshifts.front()};
-            },
-            [&mesh, &name, &solution](const auto& conductivity) -> evaluated_conductivity_t<T> {
+    for (const auto& [name, parameter] : parameters)
+        conductivity[name] = {
+            .model = parameter.model,
+            .physical = std::visit([&mesh, &name, &solution](const auto& conductivity) -> evaluated_conductivity_t<T> {
+                using Type = decltype(evaluate<T, 2u>(conductivity, {}, {}));
                 if (is_constant(conductivity))
                     return evaluate<T, 2u>(conductivity, {}, {});
                 const auto qshifts = mesh.quad_shifts(name);
-                static constexpr size_t N = std::tuple_size_v<std::remove_cvref_t<decltype(conductivity)>>;
-                metamath::types::vector_with_shifted_index<std::array<T, N>> result = {
-                    .container = std::vector<std::array<T, N>>(qshifts.size()),
+                metamath::types::vector_with_shifted_index<Type> result = {
+                    .container = std::vector<Type>(qshifts.size()),
                     .shift = qshifts.front()
                 };
                 for(size_t q = qshifts.front(); q <= qshifts.back(); ++q)
                     result[q] = evaluate<T, 2u>(conductivity, mesh.quad_coord(q), solution[q]);
                 return result;
-            }
-        }, parameter.physical.conductivity);
-
-        conductivity[name] = {
-            .model = parameter.model,
-            .physical = std::move(conduct)
+            }, parameter.physical.conductivity)
         };
-    }
     return conductivity;
 }
 
