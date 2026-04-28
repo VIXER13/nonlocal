d0e1623b8581cd6665d5580b3178971d088e27ed
VIXER13 <Zokolof@yandex.ru>
2026-04-28 17:05:41 +0300

    (-) Fixed storage of elements

--- libraries/mesh/mesh_2d/elements_set.hpp
+++ libraries/mesh/mesh_2d/elements_set.hpp
@@ -9,11 +9,6 @@ using element_integrate_1d = metamath::finite_element::element_1d_integrate<T>;
 template<class T>
 using element_integrate_2d = metamath::finite_element::element_2d_integrate<T>;
 
-template<class T>
-using finite_element_1d_sptr = std::shared_ptr<element_integrate_1d<T>>;
-template<class T>
-using finite_element_2d_sptr = std::shared_ptr<element_integrate_2d<T>>;
-
 enum class element_1d_t : uint8_t {
     LINEAR,
     QUADRATIC
@@ -30,8 +25,8 @@ enum class element_2d_t : uint8_t {
 template<class T>
 class elements_set {
 protected:
-    std::vector<finite_element_1d_sptr<T>> _elements_1d;
-    std::vector<finite_element_2d_sptr<T>> _elements_2d;
+    std::vector<element_integrate_1d<T>> _elements_1d;
+    std::vector<element_integrate_2d<T>> _elements_2d;
     const std::unordered_map<size_t, element_1d_t> _model_to_local_1d;
     const std::unordered_map<size_t, element_2d_t> _model_to_local_2d;
     const std::vector<size_t> _local_to_model_1d;
@@ -45,8 +40,8 @@ protected:
         return result;
     }
 
-    explicit elements_set(std::vector<finite_element_1d_sptr<T>>&& elements_1d,
-                          std::vector<finite_element_2d_sptr<T>>&& elements_2d,
+    explicit elements_set(std::vector<element_integrate_1d<T>>&& elements_1d,
+                          std::vector<element_integrate_2d<T>>&& elements_2d,
                           std::unordered_map<size_t, element_1d_t>&& model_to_local_1d,
                           std::unordered_map<size_t, element_2d_t>&& model_to_local_2d)
         : _elements_1d{std::move(elements_1d)}
@@ -60,19 +55,19 @@ public:
     virtual ~elements_set() noexcept = default;
 
     element_integrate_1d<T>& element_1d(const element_1d_t local) {
-        return *_elements_1d[size_t(local)];
+        return _elements_1d[size_t(local)];
     }
 
     const element_integrate_1d<T>& element_1d(const element_1d_t local) const {
-        return *_elements_1d[size_t(local)];
+        return _elements_1d[size_t(local)];
     }
 
     element_integrate_2d<T>& element_2d(const element_2d_t local) {
-        return *_elements_2d[size_t(local)];
+        return _elements_2d[size_t(local)];
     }
 
     const element_integrate_2d<T>& element_2d(const element_2d_t local) const {
-        return *_elements_2d[size_t(local)];
+        return _elements_2d[size_t(local)];
     }
 
     element_1d_t model_to_local_1d(const size_t model) const {
--- libraries/mesh/mesh_2d/vtk_elements_set.hpp
+++ libraries/mesh/mesh_2d/vtk_elements_set.hpp
@@ -24,50 +24,34 @@ class vtk_elements_set final : public elements_set<T> {
     template<class U, size_t N>
     using gauss = metamath::finite_element::gauss<U, N>;
 
-    template<class U>
-    using element_1d_integrate = metamath::finite_element::element_1d_integrate<U>;
     template<class U, size_t N>
     using lagrangian_element_1d = metamath::finite_element::lagrangian_element_1d<U, N>;
     template<class U, size_t Order>
     using element_1d = metamath::finite_element::element_1d<U, lagrangian_element_1d, Order>;
 
-    template<class U>
-    using element_2d_integrate = metamath::finite_element::element_2d_integrate<U>;
     template<class U, size_t Order>
     using triangle = metamath::finite_element::triangle<U, Order>;
+    template<class U, size_t Order>
+    using serendipity = metamath::finite_element::serendipity<U, Order>;
     template<class U, size_t N, size_t M>
     using lagrangian_element_2d = metamath::finite_element::lagrangian_element_2d<U, N, M>;
     template<class U, template<class, auto...> class Element_Type, auto... Args>
     using element_2d = metamath::finite_element::element_2d<U, Element_Type, Args...>;
 
-    static std::vector<finite_element_1d_sptr<T>> make_default_1d_elements() {
+    static std::vector<element_integrate_1d<T>> make_default_1d_elements() {
         return {
-            std::make_shared<element_1d_integrate<T>>(
-                std::make_unique<element_1d<T, 1>>(),
-                quadrature<T, gauss, 1>{}),
-            std::make_shared<element_1d_integrate<T>>(
-                std::make_unique<element_1d<T, 2>>(),
-                quadrature<T, gauss, 2>{})
+            element_integrate_1d<T>{std::make_unique<element_1d<T, 1>>(), quadrature<T, gauss, 1>{}},
+            element_integrate_1d<T>{std::make_unique<element_1d<T, 2>>(), quadrature<T, gauss, 2>{} }
         };
     }
 
-    static std::vector<finite_element_2d_sptr<T>> make_default_2d_elements() {
+    static std::vector<element_integrate_2d<T>> make_default_2d_elements() {
         return {
-            std::make_shared<element_2d_integrate<T>>(
-                std::make_unique<element_2d<T, triangle, 1>>(),
-                quadrature<T, gauss, 1>{}),
-            std::make_shared<element_2d_integrate<T>>(
-                std::make_unique<element_2d<T, triangle, 2>>(),
-                quadrature<T, gauss, 2>{}),
-            std::make_shared<element_2d_integrate<T>>(
-                std::make_unique<element_2d<T, metamath::finite_element::serendipity, 1>>(),
-                quadrature<T, gauss, 2>{}),
-            std::make_shared<element_2d_integrate<T>>(
-                std::make_unique<element_2d<T, metamath::finite_element::serendipity, 2>>(),
-                quadrature<T, gauss, 3>{}),
-            std::make_shared<element_2d_integrate<T>>(
-                std::make_unique<element_2d<T, lagrangian_element_2d, 2, 2>>(),
-                quadrature<T, gauss, 3>{})
+            element_integrate_2d<T>{ std::make_unique<element_2d<T, triangle, 1>>(), quadrature<T, gauss, 1>{}},
+            element_integrate_2d<T>{std::make_unique<element_2d<T, triangle, 2>>(), quadrature<T, gauss, 2>{}},
+            element_integrate_2d<T>{std::make_unique<element_2d<T, serendipity, 1>>(), quadrature<T, gauss, 2>{}},
+            element_integrate_2d<T>{std::make_unique<element_2d<T, serendipity, 2>>(), quadrature<T, gauss, 3>{}},
+            element_integrate_2d<T>{std::make_unique<element_2d<T, lagrangian_element_2d, 2, 2>>(), quadrature<T, gauss, 3>{}}
         };
     }
 
