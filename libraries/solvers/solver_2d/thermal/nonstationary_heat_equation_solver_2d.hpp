63f1afe7bedd1d7dab5d634683230e46d5045ef4
VIXER13 <Zokolof@yandex.ru>
2026-08-07 16:38:49 +0300

    (+) thermal nonstationary problem restored

--- libraries/solvers/solver_2d/thermal/nonstationary_heat_equation_solver_2d.hpp
+++ libraries/solvers/solver_2d/thermal/nonstationary_heat_equation_solver_2d.hpp
@@ -4,9 +4,10 @@
 #include "heat_capacity_matrix_2d.hpp"
 #include "convection_condition_2d.hpp"
 #include "radiation_condition_2d.hpp"
+#include "init_problem_settings.hpp"
 #include "thermal_parameters_2d.hpp"
 
-#include <solvers/slae/conjugate_gradient.hpp>
+#include <solvers/slae/init_solver.hpp>
 #include <solvers/solver_2d/base/boundary_condition_first_kind_2d.hpp>
 #include <solvers/solver_2d/base/boundary_condition_second_kind_2d.hpp>
 #include <solvers/solver_2d/base/right_part_2d.hpp>
@@ -15,13 +16,11 @@ namespace nonlocal::solver_2d::thermal {
 
 template<std::floating_point T>
 class nonstationary_heat_equation_solver_2d final {
-    static constexpr size_t DoF = 1;
-
     std::shared_ptr<mesh::mesh_2d<T>> _mesh;
-    std::unique_ptr<slae::conjugate_gradient<T>> slae_solver;
-    heat_capacity_matrix_2d<T> _capacity;
+    std::unique_ptr<slae::iterative_solver_base<T>> _slae_solver;
     conductivity_matrix_2d<T> _conductivity;
     metamath::linear::sparse_matrix<T> _boundary_matrix;
+    heat_capacity_matrix_2d<T> _capacity;
     std::vector<T> _right_part;
     std::vector<T> _temperature_curr;
     std::vector<T> _temperature_next;
@@ -31,6 +30,7 @@ class nonstationary_heat_equation_solver_2d final {
     evaluated_conductivity_2d<T> _parameters;
     T _time_step = T{1};
     T _time = T{0};
+    bool is_symmetric = false;
 
 public:
     explicit nonstationary_heat_equation_solver_2d(const std::shared_ptr<mesh::mesh_2d<T>>& mesh);
@@ -45,7 +45,7 @@ public:
                  const T time_step, 
                  const std::function<T(const std::array<T, 2>&)>& right_part = nullptr,
                  const std::function<T(const std::array<T, 2>&)>& init_dist = nullptr, 
-                 const T time = T{0});
+                 const T initial_time = T{0});
 
     void calc_step();
 };
@@ -53,8 +53,8 @@ public:
 template<std::floating_point T>
 nonstationary_heat_equation_solver_2d<T>::nonstationary_heat_equation_solver_2d(const std::shared_ptr<mesh::mesh_2d<T>>& mesh)
     : _mesh{mesh}
-    , _conductivity{*mesh}
-    , _capacity{*mesh}
+    , _conductivity{*_mesh}
+    , _capacity{*_mesh}
     , _right_part(mesh->container().nodes_count(), T{0})
     , _temperature_curr(mesh->container().nodes_count(), T{0})
     , _temperature_next(mesh->container().nodes_count(), T{0}) {}
@@ -88,46 +88,50 @@ void nonstationary_heat_equation_solver_2d<T>::compute(const parameters_2d<T>& p
                                                        const T time_step, 
                                                        const std::function<T(const std::array<T, 2>&)>& right_part,
                                                        const std::function<T(const std::array<T, 2>&)>& init_dist, 
-                                                       const T time) {
-    // TODO: Rework nonstationary solver
+                                                       const T initial_time) {
+    _time_step = time_step;
+    _time = initial_time;
+    _boundaries_conditions = std::move(boundaries_conditions);
 
-    // std::vector<T> solution(_conductivity.mesh().quad_shift(_conductivity.mesh().container().elements_2d_count()), T{0});
-    // const auto conductivity_parameters = evaluate_conductivity(_conductivity.mesh(), parameters, solution);
-    // solution = {};
+    if (init_dist)
+        for(const size_t node : _mesh->container().nodes())
+            _temperature_next[node] = init_dist(_mesh->container().node_coord(node));
+    _parameters = evaluate_conductivity(*_mesh, parameters, mesh::utils::nodes_to_qnodes<T>(*_mesh, _temperature_next));
 
-    // const std::vector<bool> is_inner = utils::inner_nodes(_conductivity.mesh().container(), boundaries_conditions);
-    // _conductivity.compute(conductivity_parameters, is_inner);
-    // convection_condition_2d(_conductivity.matrix().inner(), _conductivity.mesh(), boundaries_conditions, is_inner);
-    // _capacity.calc_matrix(parameters, is_inner);
+    static constexpr bool Is_Stationary = false;
+    auto settings = init_problem_settings(_mesh->container(), parameters, boundaries_conditions, Is_Stationary);
+    log_problem_settings(settings);
+    _conductivity.compute(_parameters, settings);
+    convection_condition_2d(_conductivity.matrix(), settings, *_mesh, _boundaries_conditions);
+    _conductivity.matrix() *= time_step;
+    _boundary_matrix = get_first_kind_matrix(_conductivity.matrix(), settings.is_inner_nodes, settings.is_symmetric());
+    remove_first_kind_elements(_conductivity.matrix(), settings.is_inner_nodes);
 
-    // _conductivity.matrix().inner() *= time_step();
-    // _conductivity.matrix().bound() *= time_step();
-    // _conductivity.matrix().inner() += _capacity.matrix().inner();
-    // first_kind_filler(_conductivity.mesh().process_nodes(), is_inner, [&matrix = _conductivity.matrix().inner()](const size_t row) {
-    //     matrix.values[matrix.portrait.shifts[row]] = T{1};
-    // });
+    _slae_solver = slae::init_iterative_solver(_conductivity.matrix(), settings.is_symmetric());
+    
+    const auto theroires_setter = std::views::all(_mesh->container().groups_2d()) |
+                                  std::views::transform([](const std::string& group) { return std::pair{group, theory_t::LOCAL}; });
+    settings.theories = std::unordered_map<std::string, theory_t>(theroires_setter.begin(), theroires_setter.end());
+    _capacity.compute(parameters, settings);
+    static constexpr bool Set_Diagonal = false;
+    remove_first_kind_elements(_capacity.matrix(), settings.is_inner_nodes, Set_Diagonal);
 
-    // for(const size_t node : _conductivity.mesh().container().nodes())
-    //     _temperature_next[node] = init_dist(_conductivity.mesh().container().node_coord(node));
-
-    // slae_solver = std::make_unique<slae::conjugate_gradient<T>>(_conductivity.matrix().inner());
+    _conductivity.matrix() += _capacity.matrix();
 }
 
 template<std::floating_point T>
 void nonstationary_heat_equation_solver_2d<T>::calc_step() {
-    // std::fill(_right_part.begin(), _right_part.end(), T{0});
-    // _temperature_curr.swap(_temperature_next);
-    // radiation_condition_2d(_conductivity.matrix().inner(), _right_part, _conductivity.mesh(), boundaries_conditions, 
-    //                        _temperature_curr, time_step());
-
-    // boundary_condition_second_kind_2d(_right_part, _conductivity.mesh(), boundaries_conditions);
-    // integrate_right_part<DoF>(_right_part, _conductivity.mesh(), right_part);
-
-    // using namespace metamath::operators;
-    // _right_part *= time_step();
-    // _right_part += _capacity.matrix().inner().template self_adjoint<metamath::linear::matrix_part::Upper>() * _temperature_curr;
-    // boundary_condition_first_kind_2d(_right_part, _conductivity.mesh(), boundaries_conditions, _conductivity.matrix().bound());
-    // _temperature_next = slae_solver->solve(_right_part, _temperature_curr);
+    std::fill(_right_part.begin(), _right_part.end(), T{0});
+    _temperature_curr.swap(_temperature_next);
+    // TODO: radiation condition
+    boundary_condition_second_kind_2d(_right_part, _conductivity.mesh(), _boundaries_conditions);
+    if (_inner_flux)
+        integrate_right_part(_right_part, _conductivity.mesh(), _inner_flux);
+    using namespace metamath::operators;
+    _right_part *= time_step();
+    _right_part += _capacity.matrix().template self_adjoint<metamath::linear::matrix_part::Upper>() * _temperature_curr;
+    _right_part -= _boundary_matrix * calc_first_kind_vector(_mesh->container(), _boundaries_conditions);
+    _temperature_next = _slae_solver->solve(_right_part, _temperature_curr);
 }
 
 }
