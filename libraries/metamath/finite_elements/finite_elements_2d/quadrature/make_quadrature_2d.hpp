542baf5038795dae5fd46586f9ee9272a857cbe1
VIXER13 <Zokolof@yandex.ru>
2026-08-11 19:30:34 +0300

    (+) Added barycentric quadratures

--- libraries/metamath/finite_elements/finite_elements_2d/quadrature/make_quadrature_2d.hpp
+++ libraries/metamath/finite_elements/finite_elements_2d/quadrature/make_quadrature_2d.hpp
@@ -1,10 +1,34 @@
 #pragma once
 
-#include "cartesian_production.hpp"
+#include "quadrature_2d.hpp"
+#include "primitives/barycentric.hpp"
 
 #include <metamath/finite_elements/finite_elements_1d/quadrature/make_quadrature_1d.hpp>
 
 namespace metamath::finite_element {
+
+enum class geometry_t : uint8_t {
+    Rectangle,
+    Triangle
+};
+
+template<std::floating_point T, size_t Order>
+std::unique_ptr<quadrature_2d_base<T>> make_barycentric_quadrature_2d() {
+    return std::make_unique<quadrature_2d<T, barycentric_quadrature, Order>>();
+}
+
+template<std::floating_point T>
+std::unique_ptr<quadrature_2d_base<T>> make_barycentric_quadrature_2d(const size_t order) {
+    switch(order) {
+        case 1: return make_barycentric_quadrature_2d<T, 1>();
+        case 2: return make_barycentric_quadrature_2d<T, 2>();
+        case 3: return make_barycentric_quadrature_2d<T, 3>();
+        case 4: return make_barycentric_quadrature_2d<T, 4>();
+        case 5: return make_barycentric_quadrature_2d<T, 5>();
+        case 6: return make_barycentric_quadrature_2d<T, 6>();
+        default: throw std::domain_error{"Unsupported quadrature order " + std::to_string(order)};
+    }
+}
 
 template<std::floating_point T>
 std::unique_ptr<quadrature_2d_base<T>> make_quadrature_2d(const size_t order_x, const size_t order_y) {
@@ -12,9 +36,14 @@ std::unique_ptr<quadrature_2d_base<T>> make_quadrature_2d(const size_t order_x, 
 }
 
 template<std::floating_point T>
-std::unique_ptr<quadrature_2d_base<T>> make_quadrature_2d(const size_t order) {
-    const auto quadrature_1d = make_quadrature_1d<T>(order);
-    return std::make_unique<quadrature_2d<T, cartesian_production>>(*quadrature_1d, *quadrature_1d);
+std::unique_ptr<quadrature_2d_base<T>> make_quadrature_2d(const size_t order, const geometry_t geometry) {
+    if (geometry == geometry_t::Triangle)
+        return make_barycentric_quadrature_2d<T>(order);
+    else if (geometry == geometry_t::Rectangle) {
+        const auto quadrature_1d = make_quadrature_1d<T>(order);
+        return std::make_unique<quadrature_2d<T, cartesian_production>>(*quadrature_1d, *quadrature_1d);
+    } else
+        throw std::domain_error{"Unsupported geometry type"};
 }
 
 }
--- tests/finite_elements/quadrature_2d_test.cpp
+++ tests/finite_elements/quadrature_2d_test.cpp
@@ -11,34 +11,39 @@ using namespace metamath::finite_element;
 using T = double;
 
 const suite<"quadrature_2d"> _ = [] {
-    for(const size_t order : std::ranges::iota_view{1zu, 6zu}) {
-        const std::string suffix = "_order_" + std::to_string(order);
-        const auto quadrature = make_quadrature_2d<T>(order);
+    for(const auto type : {geometry_t::Rectangle, geometry_t::Triangle})
+        for(const size_t order : std::ranges::iota_view{1zu, 7zu}) {
+            if (type == geometry_t::Rectangle && order > 5)
+                continue;
+            const std::string type_name = type == geometry_t::Rectangle ? "_rectangle" : "_triangle";
+            const std::string suffix = type_name + "_order_" + std::to_string(order);
+            const auto quadrature = make_quadrature_2d<T>(order, type);
 
-        test("nodes_count" + suffix) = [&quadrature, order] {
-            expect(eq(quadrature->nodes_count(), order * order)) << "Unexpected nodes count.";
-        };
-
-        test("weights_sum" + suffix) = [&quadrature] {
-            const auto nodes = quadrature->nodes();
-            const auto weight_summator = [&quadrature](const T sum, const size_t node) {
-                return sum + quadrature->weight(node);
+            test("nodes_count" + suffix) = [&quadrature, type, order] {
+                static constexpr auto Nodes_Count = std::array{1zu, 3zu, 4zu, 6zu, 7zu, 12zu};
+                expect(eq(quadrature->nodes_count(), type == geometry_t::Triangle ? Nodes_Count[order - 1] : order * order)) << "Unexpected nodes count.";
             };
-            const T weights_sum = std::accumulate(nodes.begin(), nodes.end(), T{0}, weight_summator);
-            static constexpr T Epsilon = 9e-16;
-            expect(approx(weights_sum, dynamic_cast<const geometry_2d_base<T>&>(*quadrature).area(), Epsilon)) << "Unexpected weights sum.";
-        };
 
-        test("copy" + suffix) = [&quadrature] {
-            const auto copied_quadrature = quadrature->copy();
-            expect(eq(copied_quadrature->nodes_count(), quadrature->nodes_count())) << "Unexpected nodes count in copied quadrature.";
-            for(const size_t i : quadrature->nodes()) {
-                expect(eq(copied_quadrature->weight(i), quadrature->weight(i))) << "Unexpected weight in copied quadrature.";
-                expect(eq(copied_quadrature->node(i)[0], quadrature->node(i)[0])) << "Unexpected node coordinate 0 in copied quadrature.";
-                expect(eq(copied_quadrature->node(i)[1], quadrature->node(i)[1])) << "Unexpected node coordinate 1 in copied quadrature.";
-            }
-        };
-    }
+            test("weights_sum" + suffix) = [&quadrature] {
+                const auto nodes = quadrature->nodes();
+                const auto weight_summator = [&quadrature](const T sum, const size_t node) {
+                    return sum + quadrature->weight(node);
+                };
+                const T weights_sum = std::accumulate(nodes.begin(), nodes.end(), T{0}, weight_summator);
+                static constexpr T Epsilon = 1e-15;
+                expect(approx(weights_sum, dynamic_cast<const geometry_2d_base<T>&>(*quadrature).area(), Epsilon)) << "Unexpected weights sum.";
+            };
+
+            test("copy" + suffix) = [&quadrature] {
+                const auto copied_quadrature = quadrature->copy();
+                expect(eq(copied_quadrature->nodes_count(), quadrature->nodes_count())) << "Unexpected nodes count in copied quadrature.";
+                for(const size_t i : quadrature->nodes()) {
+                    expect(eq(copied_quadrature->weight(i), quadrature->weight(i))) << "Unexpected weight in copied quadrature.";
+                    expect(eq(copied_quadrature->node(i)[0], quadrature->node(i)[0])) << "Unexpected node coordinate 0 in copied quadrature.";
+                    expect(eq(copied_quadrature->node(i)[1], quadrature->node(i)[1])) << "Unexpected node coordinate 1 in copied quadrature.";
+                }
+            };
+        }
 };
 
 }
