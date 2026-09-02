450722519e53ec161e07a02c9091ffb4a7f15642
AndreySokolov <Zokolof@yandex.ru>
2026-09-02 10:26:19 +0300

    (+) Added nonlocal experiment

--- libraries/solvers/solver_2d/mechanical/equilibrium_equation_2d.hpp
+++ libraries/solvers/solver_2d/mechanical/equilibrium_equation_2d.hpp
@@ -14,6 +14,7 @@
 #include <solvers/solver_2d/base/right_part_2d.hpp>
 
 #include <optional>
+#include <iostream>
 
 namespace nonlocal::solver_2d::mechanical {
 
@@ -36,7 +37,8 @@ mechanical::mechanical_solution_2d<T> equilibrium_equation(const std::shared_ptr
                                                            const raw_mechanical_parameters<T>& parameters,
                                                            const mechanical_boundaries_conditions_2d<T>& boundaries_conditions,
                                                            const std::vector<T>& delta_temperature = {},
-                                                           const std::function<std::array<T, 2>(const std::array<T, 2>&)>& right_part = nullptr) {
+                                                           const std::function<std::array<T, 2>(const std::array<T, 2>&)>& right_part = nullptr,
+                                                           const bool use_preconditioner = true) {
     const auto settings = init_problem_settings(mesh->container(), parameters, boundaries_conditions);
     log_problem_settings(settings);
     const auto evaluated_parameters = evaluate_mechanical_parameters(*mesh, parameters, delta_temperature);
@@ -51,10 +53,13 @@ mechanical::mechanical_solution_2d<T> equilibrium_equation(const std::shared_ptr
     boundary_condition_first_kind_2d(stiffness.matrix(), f, settings, mesh->container(), boundaries_conditions);
 
     auto solver = slae::init_iterative_solver(stiffness.matrix(), settings.is_symmetric());
-    if (settings.is_nonlocal())
+    if (use_preconditioner && settings.is_nonlocal())
         solver->preconditioner(init_preconditioner(settings, *mesh, evaluated_parameters, boundaries_conditions));
     auto solution = mechanical_solution_2d{mesh, evaluated_parameters, solver->solve(f)};
     solution.calc_strain_and_stress(evaluated_parameters);
+
+    std::cerr << "Iterations = " << solver->iterations() << std::endl;
+
     return solution;
 }
 
